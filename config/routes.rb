2bf5a9574c4d625dbc042672128c4ce9011acd09
SahilThakur7 <sahilrd07@gmail.com>
2017-06-13 11:54:30 +0530

    Sign in with just email

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     root to: "todos#index"
     resources :todos
+    resource :session, only: [:new,:create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
