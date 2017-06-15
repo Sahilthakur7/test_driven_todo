b1821a2021f1052eeff4a57a42b815f7335579fb
SahilThakur7 <sahilrd07@gmail.com>
2017-06-15 13:09:36 +0530

    First unit test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
     root to: "todos#index"
-    resources :todos
+    resources :todos do
+        resource :completion, only: [:create]
+    end
     resource :session, only: [:new,:create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
